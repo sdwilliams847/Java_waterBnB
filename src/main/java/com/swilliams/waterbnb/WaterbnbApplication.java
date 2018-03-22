2485aae42696f6fb7ec1d74ccc23deb7c154513f
sdwilliams847 <spencer.williams734@yahoo.com>
2018-03-22 16:03:50 -0400

    first Commit

---
+++ src/main/java/com/swilliams/waterbnb/WaterbnbApplication.java
@@ -0,0 +1,12 @@
+package com.swilliams.waterbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterbnbApplication.class, args);
+	}
+}
